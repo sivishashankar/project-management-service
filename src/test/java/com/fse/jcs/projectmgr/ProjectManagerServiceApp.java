314f0b9e12950cc507e92dd91f9d24bf018b6ae1
Admin <sivishashankar@gmail.com>
2019-07-31 18:35:50 +0530

    Created project management maven project and added spring boot main class

---
+++ src/test/java/com/fse/jcs/projectmgr/ProjectManagerServiceApp.java
@@ -0,0 +1,13 @@
+package com.fse.jcs.projectmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerServiceApp 
+{
+    public static void main( String[] args )
+    {
+       SpringApplication.run(ProjectManagerServiceApp.class, args);
+    }
+}
